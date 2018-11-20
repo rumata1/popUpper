848e0c2fa5373a88f03a7f9a9d032a180eedb68a
rumata1 <borovikov-ao@yandex.ru>
2018-11-20 12:56:35 +0500

    Несколько поп-апов не конфликтуют между собой

--- popUpper.js
+++ popUpper.js
@@ -1,63 +1,64 @@
     //По клику на вызове указанному в параметрах popUp-контейнеру придается класс active
     //В дом дереве кнопки вызова должны быть в том же порядке, что и контейнеры
+	//несколько серий поп-апов не конфликтуют с друг другом
     
     /*
     Чтобы вызвать поп-ап, нужно прописать
     
-    popUpper({
+    new popUpper({
         popBtn: 'класс кнопок вызова поп-апов',
         popBtnClose: 'класс кнопок закрытия поп-апов', 
         popUp: 'класс контейнеров поп-апов',
         callback: 'функция для коллбеков после вызова поп-апа'
-        oneForAll: 'По умолчанию false, true - только одно окно поп-апа'
-        
     });
     
     */
 
-    function popUpper({popBtn, popBtnClose, popUp, oneForAll, callback}){
-        this.popBtn = document.querySelectorAll(popBtn);
-        this.popUp = document.querySelectorAll(popUp);
-        this.popBtnClose = document.querySelectorAll(popBtnClose);
-        this.callback = callback || function(){}
+    function popUpper({popBtn, popBtnClose, popUp, pointer, callback, oneForAll}){
         
+		let paramsObj = arguments;
+		paramsObj.popBtn = document.querySelectorAll(popBtn);
+        paramsObj.popUp = document.querySelectorAll(popUp);
+        paramsObj.popBtnClose = document.querySelectorAll(popBtnClose);
+        paramsObj.callback = callback || function(){};
+		
         window.addEventListener('load',()=>{
-            start();    
+			start();    
         });   
         
         function start(){
-            this.open =(index)=>{
-                if(oneForAll){
-                    this.popUp[0].classList.add('active');    
+            paramsObj.open =(index)=>{
+				if(oneForAll){
+                     paramsObj.popUp[0].classList.add('active');     
                 }
                 else {
-                    this.popUp[index].classList.add('active');     
+                     paramsObj.popUp[index].classList.add('active');      
                 }
                 
-                this.callback();
+                paramsObj.callback();     
             }
             
-            this.close =(index)=>{
-                if(oneForAll){
-                    this.popUp[0].classList.remove('active');    
+            paramsObj.close =(index)=>{
+				if(oneForAll){
+                    paramsObj.popUp[0].classList.remove('active');    
                 }
                 else {
-                    this.popUp[index].classList.remove('active');      
+                    paramsObj.popUp[index].classList.remove('active');       
                 } 
             }
             
-            for(let i = 0; i < this.popBtn.length; i++){
-                this.popBtn[i].addEventListener('click',()=>{
-                    this.open(i);      
+            for(let i = 0; i < paramsObj.popBtn.length; i++){
+                paramsObj.popBtn[i].addEventListener('click',()=>{
+                    paramsObj.open(i);      
                 });
             
             }
             
-            for(let i = 0; i < this.popBtnClose.length; i++){
-                this.popBtnClose[i].addEventListener('click',()=>{
-                    this.close(i);      
+            for(let i = 0; i < paramsObj.popBtnClose.length; i++){
+                paramsObj.popBtnClose[i].addEventListener('click',()=>{
+                    paramsObj.close(i);      
                 });
             
-            }  
+            } 
         } 
     }
